ebaf79ee8f904f29e87ab7378a9ae78caee6863a
leesomin1204 <sm1157@naver.com>
2025-07-11 10:53:11 +0900

    07.11

--- 11 금융 OpenAPI 활용/source/exam01/exam01/src/main/java/org/koreait/member/controllers/MemberController.java
+++ 11 금융 OpenAPI 활용/source/exam01/exam01/src/main/java/org/koreait/member/controllers/MemberController.java
@@ -1,30 +1,33 @@
 package org.koreait.member.controllers;
 
 import jakarta.validation.Valid;
-import org.springframework.http.ResponseEntity;
+import org.koreait.global.exceptions.BadRequestException;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/member")
 public class MemberController {
 
     @PostMapping("/join")
-    public ResponseEntity<Object> join(@RequestBody @Valid RequestJoin form, Errors errors) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void join(@RequestBody @Valid RequestJoin form, Errors errors) {
         if (errors.hasErrors()) {
-            List<String> errorMessage = errors.getFieldErrors().stream().map(e->e.getDefaultMessage()).toList();
-            // return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
-            return ResponseEntity.badRequest().body(errorMessage);
-        }
+            throw new BadRequestException(errors);
 
-        // return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-        return ResponseEntity.noContent().build();
+        }
     }
+//    public ResponseEntity<Object> join(@RequestBody @Valid RequestJoin form, Errors errors) {
+//        if (errors.hasErrors()) {
+//            List<String> errorMessags = errors.getFieldErrors().stream().map(e -> e.getDefaultMessage()).toList();
+//            //return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessags);
+//            return ResponseEntity.badRequest().body(errorMessags);
+//        }
+//
+//        //return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+//        return ResponseEntity.noContent().build();
+//    }
 
     @PostMapping("/login")
     public void login() {
@@ -33,6 +36,9 @@ public class MemberController {
             throw new IllegalArgumentException("잘못된 요청...");
         }
     }
+
+
+
 //    public ResponseEntity<Object> join(@RequestBody @Valid RequestJoin form, Errors errors) {
 //
 //        HttpHeaders headers = new HttpHeaders();
@@ -41,7 +47,7 @@ public class MemberController {
 //
 //        ResponseEntity<Object> res = new ResponseEntity<>(headers, HttpStatus.NO_CONTENT);
 //
-//        if(errors.hasErrors()) {
+//        if (errors.hasErrors()) {
 //            List<String> errorMessages = errors.getFieldErrors().stream().map(e -> e.getDefaultMessage()).toList();
 //
 //            res = new ResponseEntity<>(errorMessages, HttpStatus.BAD_REQUEST);
